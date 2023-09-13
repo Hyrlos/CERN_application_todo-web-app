1d09b0f17536226c74c6f41c80f53280c2eab815
Hyrlos <thomas.georges@lirmm.fr>
2023-09-13 22:09:48 +0200

    Tests TaskCategoryController

--- src/main/java/ch/cern/todo/controller/TaskCategoryController.java
+++ src/main/java/ch/cern/todo/controller/TaskCategoryController.java
@@ -1,8 +1,11 @@
 package ch.cern.todo.controller;
 
+import ch.cern.todo.database.LoadDatabase;
 import ch.cern.todo.entity.TaskCategory;
 import ch.cern.todo.exception.TaskCategoryNotFoundException;
 import ch.cern.todo.repository.TaskCategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +13,7 @@ import java.util.List;
 @RestController
 public class TaskCategoryController {
         private final TaskCategoryRepository tcRepository;
+        private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
         TaskCategoryController(TaskCategoryRepository tcRepository) {
                 this.tcRepository = tcRepository;
@@ -17,43 +21,53 @@ public class TaskCategoryController {
 
         // Get ALL
         @GetMapping("/taskCategory")
-        List<TaskCategory> all() {
+        List<TaskCategory> getAllTaskCategory() {
+                log.debug("Get all TaskCategory");
                 return tcRepository.findAll();
         }
 
         // Get category by id
         @GetMapping("/taskCategory/{categoryId}")
-        TaskCategory one(@PathVariable Long categoryId) {
-
-                return tcRepository.findById(categoryId)
+        TaskCategory getOneTaskCategory(@PathVariable Long categoryId) {
+                TaskCategory tcGet = tcRepository.findById(categoryId)
                         .orElseThrow(() -> new TaskCategoryNotFoundException(categoryId));
+
+                log.debug("Get TaskCategory " + categoryId + ": " + tcGet);
+                return tcGet;
         }
 
 
-                // Post to create
+        // Post to create
         @PostMapping("/taskCategory")
         TaskCategory newTaskCategory(@RequestBody TaskCategory newTaskCategory) {
-                return tcRepository.save(newTaskCategory);
+                TaskCategory tcPosted = tcRepository.save(newTaskCategory);
+                log.debug("Post TaskCategory: " + tcPosted);
+                return tcPosted;
         }
 
         // Edit
         @PutMapping("/taskCategory/{categoryId}")
-        TaskCategory replaceEmployee(@RequestBody TaskCategory newTaskCategory, @PathVariable Long categoryId) {
+        TaskCategory replaceTaskCategory(@RequestBody TaskCategory newTaskCategory, @PathVariable Long categoryId) {
 
                 return tcRepository.findById(categoryId)
                         .map(taskCategory -> {
                                 taskCategory.setCategoryName(newTaskCategory.getCategoryName());
                                 taskCategory.setCategoryDescription(newTaskCategory.getCategoryDescription());
-                                return tcRepository.save(taskCategory);
+                                TaskCategory tcPutted = tcRepository.save(taskCategory);
+                                log.debug("Put TaskCategory: " + tcPutted);
+                                return tcPutted;
                         })
                         .orElseGet(() -> { // If taskCategory not found a new taskCategory is created with the provided id
-                                return tcRepository.save(newTaskCategory);
+                                TaskCategory tcPutted = tcRepository.save(newTaskCategory);
+                                log.debug("Put TaskCategory: " + tcPutted);
+                                return tcPutted;
                         });
         }
 
         // Delete one
         @DeleteMapping("/taskCategory/{categoryId}")
         void deleteTaskCategory(@PathVariable Long categoryId) {
+                log.debug("Delete TaskCategory " + categoryId + ": " + tcRepository.findById(categoryId));
                 tcRepository.deleteById(categoryId);
         }
 }
