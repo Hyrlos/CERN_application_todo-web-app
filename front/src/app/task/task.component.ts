3427d0bbf99e3fd8e3e6c8be2fe83f67a23b3dfc
Hyrlos <thomas.georges@lirmm.fr>
2023-09-20 00:04:15 +0200

    Add delete taskcategory

--- front/src/app/task/task.component.ts
+++ front/src/app/task/task.component.ts
@@ -2,44 +2,75 @@ import {Component, Injectable, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Task} from './ITask';
 import {Observable} from "rxjs";
+import {TaskCategory} from "../task-category/ITaskCategory";
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 @Component({
-    selector: 'app-task',
-    templateUrl: './task.component.html',
-    styleUrls: ['./task.component.css']
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-    httpOptions = {
-        headers: new HttpHeaders({'Content-Type': 'application/json'})
-    };
-    urlTask: string = "http://localhost:8080/task";
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+  urlTask: string = "http://localhost:8080/task";
+  urlTaskCategory: string = "http://localhost:8080/taskCategory";
 
-    taskList: Task[] = [];
+  taskList: Task[] = [];
+  taskCategoryList: TaskCategory[] = [];
 
-    constructor(
-        private http: HttpClient) {
-    }
+  selectedTaskCategoryId: number = -1; // Initialize as -1
 
-    getTask(): void {
-        this.http.get<Task[]>(this.urlTask).subscribe(data =>
-            this.taskList = data
-        );
-    }
+  constructor(
+    private http: HttpClient) {
+  }
 
-    deleteHero(id: number): Observable<Task> {
-        const url = `${this.urlTask}/${id}`;
-        return this.http.delete<Task>(url, this.httpOptions);
-    }
+  getTask(): void {
+    this.http.get<Task[]>(this.urlTask).subscribe(data =>
+      this.taskList = data
+    );
+  }
 
-    ngOnInit() {
-        this.getTask();
-    }
+  deleteTask(id: number): Observable<Task> {
+    const url = `${this.urlTask}/${id}`;
+    return this.http.delete<Task>(url, this.httpOptions);
+  }
 
-    delete(task: Task) {
-        this.taskList = this.taskList.filter(t => t !== task);
-        this.deleteHero(task.taskId).subscribe();
-    }
+  ngOnInit() {
+    this.getTask();
+    this.getTaskCategory();
+  }
+
+  getTaskCategory(): void {
+    this.http.get<TaskCategory[]>(this.urlTaskCategory).subscribe(data =>
+      this.taskCategoryList = data
+    );
+  }
+
+  delete(task: Task) {
+    this.taskList = this.taskList.filter(t => t !== task);
+    this.deleteTask(task.taskId).subscribe();
+  }
+
+  add(taskName: string, taskDescription: string, taskDeadline: string) {
+    taskName = taskName.trim();
+    taskDescription = taskDescription.trim();
+    const deadline = Date.parse(taskDeadline.trim());
+    const taskCategory = this.taskCategoryList.find(category => category.categoryId == this.selectedTaskCategoryId);
+    let taskId = 0;
+
+    let newTask = {taskId, taskName, taskDescription, deadline, taskCategory} as Task;
+    this.addTask(newTask)
+      .subscribe(task => {
+        this.taskList.push(task);
+      });
+
+  }
+
+  private addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.urlTask, task, this.httpOptions);
+  }
 }
